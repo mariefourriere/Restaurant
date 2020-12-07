a9a6e8c4dd21ef740f1586dac7ab385801b24043
JeanFabry <jeanfabry1435@gmail.com>
2020-12-07 16:13:05 +0100

    with basket bar appearing

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 let myBasketButton = document.querySelector("body > header > nav > div.nav-right.nav-menu > span > a");
 
-let clicked = false;
+let main = document.querySelector('main')
 
-const recapOrder =(e) => {
-    console.log(e);
-let recapDiv = document.createElement('div');
-recapDiv.classList.add('recapDiv');
-recapDiv.innerHTML="coucou";
-document.body.appendChild(recapDiv);
+let clicked = true;
+let recapDiv = document.createElement("div");
+recapDiv.textContent="You can find the recap of your order below:"
+
+main.appendChild(recapDiv);
+recapDiv.classList.add("recapDiv");
+
+const recapOrder = (e) => {
+
+    if (clicked) {
+        console.log("clicked:", clicked);
+        recapDiv.style.right="0";
+        clicked = false;
+    } else {
+        console.log(clicked);
+        recapDiv.style.right = "-30%";
+        clicked = true;
+    }   
 }
+
 
 myBasketButton.addEventListener("click", recapOrder);
