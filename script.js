a8a4c4cbf7d4ca939837d960e9f376c0260062bb
Jniouen-Wassim <jniouenwassim@gmail.com>
2020-12-07 22:30:58 +0100

    commit

--- script.js
+++ script.js
@@ -1,92 +1,92 @@
 const collection = [{
-    title: "Poulet rôti",
-    category: ["Plat"],
-    category2: ["Non-Vegetarien"],
-    img: "./assets/images/plat_1.png",  
-    description: "Ce poulet roti est incryable. retrouvez-y du piment et du poulet.",
-    Prix:"Ajoutez moi pour: 12€"
-},
+        title: "Poulet rôti",
+        category: ["Plat"],
+        category2: ["Non-Vegetarien"],
+        img: "./assets/images/plat_1.png",
+        description: "Ce poulet roti est incryable. retrouvez-y du piment et du poulet.",
+        Prix: "Ajoutez moi pour: 12€"
+    },
 
-{
-    title: "Tiramisu",
-    category: ["Dessert"],
-    category2: ["Vegetarien"],
-    img: "./assets/images/plat_2.png",  
-    description: "Pour réchauffer les coeurs un bon Tiramisu.",
-    Prix:"Ajoutez moi pour: 15€"
-},
+    {
+        title: "Tiramisu",
+        category: ["Dessert"],
+        category2: ["Vegetarien"],
+        img: "./assets/images/plat_2.png",
+        description: "Pour réchauffer les coeurs un bon Tiramisu.",
+        Prix: "Ajoutez moi pour: 15€"
+    },
 
-{
-    title: "Gyoza aux poireaux",
-    category: ["Plat"],
-    category2: ["Vegetarien"],
-    img: "./assets/images/plat_3.png",  
-    description: "De bons poireaux c'est tout ce qu'il vous faut",
-    Prix:"Ajoutez moi pour: 14€"
-},
+    {
+        title: "Gyoza aux poireaux",
+        category: ["Plat"],
+        category2: ["Vegetarien"],
+        img: "./assets/images/plat_3.png",
+        description: "De bons poireaux c'est tout ce qu'il vous faut",
+        Prix: "Ajoutez moi pour: 14€"
+    },
 
-{
-    title: "Steak de légumes",
-    category: ["Plat"],
-    category2: ["Vegetarien"],
-    img: "./assets/images/plat_4.png",  
-    description: "Plat 100% maison dans un petit jardin privatif.",
-    Prix:"Ajoutez moi pour: 18€"
-},
+    {
+        title: "Steak de légumes",
+        category: ["Plat"],
+        category2: ["Vegetarien"],
+        img: "./assets/images/plat_4.png",
+        description: "Plat 100% maison dans un petit jardin privatif.",
+        Prix: "Ajoutez moi pour: 18€"
+    },
 
-{
-    title: "Moscow Mule",
-    category: ["Boisson"],
-    category2: ["Vegetarien"],
-    img: "./assets/images/plat_5.png",  
-    description: "Cocktail à base de vodka, de bière de gingembre épicée et du citron vert.",
-    Prix:"Ajoutez moi pour: 17€"
-},
+    {
+        title: "Moscow Mule",
+        category: ["Boisson"],
+        category2: ["Vegetarien"],
+        img: "./assets/images/plat_5.png",
+        description: "Cocktail à base de vodka, de bière de gingembre épicée et du citron vert.",
+        Prix: "Ajoutez moi pour: 17€"
+    },
 
-{
-    title: "Steak frite",
-    category: ["Plat"],
-    category2: ["Non-Vegetarien"],
-    img: "./assets/images/plat_6.png",  
-    description: "De bonnes frites et de la bonne viande.",
-    Prix:"Ajoutez moi pour: 20€"
-},
+    {
+        title: "Steak frite",
+        category: ["Plat"],
+        category2: ["Non-Vegetarien"],
+        img: "./assets/images/plat_6.png",
+        description: "De bonnes frites et de la bonne viande.",
+        Prix: "Ajoutez moi pour: 20€"
+    },
 
-{
-    title: "Cocktail",
-    category: ["Boisson"],
-    category2: ["Vegetarien"],
-    img: "./assets/images/plat_7.png",  
-    description: "Un cocktail rafraîchissant pour l'été pour vôtre plus grand plaisir.",
-    Prix:"Ajoutez moi pour: 9€"
-},
+    {
+        title: "Cocktail",
+        category: ["Boisson"],
+        category2: ["Vegetarien"],
+        img: "./assets/images/plat_7.png",
+        description: "Un cocktail rafraîchissant pour l'été pour vôtre plus grand plaisir.",
+        Prix: "Ajoutez moi pour: 9€"
+    },
 
-{
-    title: "Poisson drésser",
-    category: ["Plat"],
-    category2: ["Non-Vegetarien"],
-    img: "./assets/images/plat_8.png",  
-    description: "Fillet de poisson accompagné de pomme de terre et de la salade.",
-    Prix:"Ajoutez moi pour: 11€"
-},
+    {
+        title: "Poisson drésser",
+        category: ["Plat"],
+        category2: ["Non-Vegetarien"],
+        img: "./assets/images/plat_8.png",
+        description: "Fillet de poisson accompagné de pomme de terre et de la salade.",
+        Prix: "Ajoutez moi pour: 11€"
+    },
 
-{
-    title: "Glace",
-    category: ["Dessert"],
-    category2: ["Vegetarien"],
-    img: "./assets/images/plat_9.png",  
-    description: "Délicieuse glace mangue framboise.",
-    Prix:"Ajoutez moi pour: 14€"
-},
+    {
+        title: "Glace",
+        category: ["Dessert"],
+        category2: ["Vegetarien"],
+        img: "./assets/images/plat_9.png",
+        description: "Délicieuse glace mangue framboise.",
+        Prix: "Ajoutez moi pour: 14€"
+    },
 
-{
-    title: "Mozzarella Tomate",
-    category: ["Dessert"],
-    category2: ["Vegetarien"],
-    img: "./assets/images/plat_10.png",  
-    description: "De simple mozzarella, mais accompagné de délicieuse rondelle de tomate.",
-    Prix:"Ajoutez moi pour: 16€"
-},
+    {
+        title: "Mozzarella Tomate",
+        category: ["Dessert"],
+        category2: ["Vegetarien"],
+        img: "./assets/images/plat_10.png",
+        description: "De simple mozzarella, mais accompagné de délicieuse rondelle de tomate.",
+        Prix: "Ajoutez moi pour: 16€"
+    },
 ];
 
 /////////////////////////////   Card   ////////////////////////////////////////////////////
@@ -114,7 +114,7 @@ for (let element of collection) {
     cardfig.appendChild(image);
 
     //============================== TAG =================================//
-   
+
     const cardCont = document.createElement('div');
     cardCont.className = "card-content";
     colonne.appendChild(cardCont);
@@ -128,9 +128,9 @@ for (let element of collection) {
     tag2.className = "tag is-danger";
     tag2.textContent = element.category2;
     cardCont.appendChild(tag2);
-       
+
     //==================================== TITRE + Texte =====================================//
-   
+
     const cardAllText = document.createElement('div');
     cardAllText.className = "content";
     cardCont.appendChild(cardAllText);
@@ -155,7 +155,7 @@ for (let element of collection) {
     cardPrix.className = "button";
     cardPrix.textContent = element.Prix;
     cardAllText.appendChild(cardPrix);
-  //ok  
+    //ok  
 
 
 
